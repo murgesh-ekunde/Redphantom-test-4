e6ff638f64ce8acb3b9edfdbd5e8beb3da1457dc
Murgesh Ekunde <mekunde@gmail.com>
2023-03-13 19:15:03 +0530

    changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -8,11 +8,11 @@ function Top() {
 
       <div className="left">
         <h1>Read to Start A Successfull Life On Belief?</h1>
-        <p>
+        <h3>
           The RedPhantom Group is a great resource for reshaping youth and
           communities toward success,with a particular emphasis on youth
           development.
-        </p>
+        </h3>
       </div>
 
       <div className="right">
